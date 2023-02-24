9bb7d25cf2dcc7fc13be709703c74b127f04367d
peiyanpan <1065112771@qq.com>
2023-02-24 09:58:19 +0800

    合并分支

---
+++ run.sh
@@ -0,0 +1,16 @@
+cd microservice/
+
+cd comment/
+go run main.go $
+
+cd ../favorite/
+go run main.go
+
+cd ../relation/
+go run main.go
+
+cd ../user/
+go run main.go
+
+cd ../video/
+go run main.go
